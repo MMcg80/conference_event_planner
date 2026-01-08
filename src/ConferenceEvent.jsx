4c00040212ce2c7f09f37769524b512658083788
MMcg80 <shirokuma025>
2026-01-08 09:33:12 -0500

    ItemsDisplay

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -46,19 +46,16 @@ const ConferenceEvent = () => {
 
     const handleMealSelection = (index) => {
         const item = mealsItems[index];
-        if (item.selected && item.type === "mealForPeople") {
+        if (item.selected) {
             // Ensure numberOfPeople is set before toggling selection
-            const newNumberOfPeople = item.selected ? numberOfPeople : 0;
-            dispatch(toggleMealSelection(index, newNumberOfPeople));
+            // const newNumberOfPeople = item.selected ? numberOfPeople : 0;
+            dispatch(toggleMealSelection(index));
         }
         else {
             dispatch(toggleMealSelection(index));
         }
     };
 
-    const getItemsFromTotalCost = () => {
-        const items = [];
-    };
 
     const getItemsFromTotalCost = () => {
         const items = [];
@@ -87,9 +84,43 @@ const ConferenceEvent = () => {
         return items;
       };
 
-    const ItemsDisplay = ({ items }) => {
-
+      const ItemsDisplay = ({ items }) => {
+        console.log(items);
+        return <>
+            <div className="display_box1">
+                {items.length === 0 && <p>No items selected</p>}
+                <table className="table_item_data">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Unit Cost</th>
+                            <th>Quantity</th>
+                            <th>Subtotal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {items.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.name}</td>
+                                <td>${item.cost}</td>
+                                <td>
+                                    {item.type === "meals" || item.numberOfPeople
+                                    ? ` For ${numberOfPeople} people`
+                                    : item.quantity}
+                                </td>
+                                <td>{item.type === "meals" || item.numberOfPeople
+                                    ? `${item.cost * numberOfPeople}`
+                                    : `${item.cost * item.quantity}`}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
     };
+
+
     const calculateTotalCost = (section) => {
         let totalCost = 0;
         if (section === "venue") {
